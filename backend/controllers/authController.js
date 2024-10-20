5a6e021ca9ca8252e5313eca06ebc80cf059f242
Shimork04 <mauryashivampersbiz@gmail.com>
2024-10-21 01:03:45 +0530

    auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require('../models/User.model');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await User.create({ name, email, password });
+        res.status(201).json({ message: 'User registered successfully', user });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await user.comparePassword(password)))
+            return res.status(401).json({ message: 'Invalid email or password' });
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+module.exports = { registerUser, loginUser };
